cef3804724312e4e14162c69e0da32df1670079f
marcus <marcuslopee@gmail.com>
2025-08-17 13:12:05 -0300

    feat: Cart Entity mapped jpa

---
+++ cart/src/main/java/com/eCommerce/cart/model/Cart.java
@@ -0,0 +1,27 @@
+package com.eCommerce.cart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "cart_total")
+    private BigDecimal cartTotal;
+}
